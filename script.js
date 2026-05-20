64aa1557d516206e2bc0f8ad34474f7a0cc5d0fa
Awawdah <60307557@udst.edu.qa>
2026-05-20 14:55:49 +0300

    Implemented the mouseover system and interactivity

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const container = document.querySelector("#container");
 
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < 100; i++) {
     const row = document.createElement("div");
     row.classList.add("row"); 
-    for (let j = 0; j < 16; j++) {
+    for (let j = 0; j < 100; j++) {
         const square = document.createElement("div");
         square.classList.add("square");
+        square.addEventListener('mouseover', e => square.style.background = "black");
         row.appendChild(square);
     }
     container.appendChild(row);
-    
 }
