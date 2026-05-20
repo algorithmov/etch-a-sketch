b23a42226c6fc45a6d2390843df16b21f5ebfda9
Awawdah <60307557@udst.edu.qa>
2026-05-20 15:37:01 +0300

    Implemented the reset technicality with a sustainably clean slate each iteration

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 const container = document.querySelector("#container");
 
-for (let i = 0; i < 100; i++) {
-    const row = document.createElement("div");
-    row.classList.add("row"); 
-    for (let j = 0; j < 100; j++) {
-        const square = document.createElement("div");
-        square.classList.add("square");
-        square.addEventListener('mouseover', e => square.style.background = "black");
-        row.appendChild(square);
+function generateGrid(x) {
+    for (let i = 0; i < x; i++) {
+        const row = document.createElement("div");
+        row.classList.add("row"); 
+        for (let j = 0; j < x; j++) {
+            const square = document.createElement("div");
+            square.classList.add("square");
+            square.addEventListener('mouseover', e => square.style.background = "black");
+            row.appendChild(square);
+        }
+        container.appendChild(row);
     }
-    container.appendChild(row);
 }
+
+generateGrid(16);
+
+document.getElementById("popup").addEventListener('click', e => {
+    let dimension = prompt("What square grid dimensions would you like (Enter one dimension)");
+    if (dimension >= 0 && dimension <= 100) {
+        while (container.hasChildNodes()) {
+            container.removeChild(container.firstChild);
+          }
+        generateGrid(dimension);
+    } else {
+        let dimension = alert("Invalid Value");
+    }
+});
